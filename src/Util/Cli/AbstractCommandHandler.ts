1c0d189daec0507186375b93a23d45ca7adf8f0f
mararok <5163714+Mararok@users.noreply.github.com>
2024-10-02 19:55:12 +0200

    fix: fix handling args in command handlers

--- src/Util/Cli/AbstractCommandHandler.ts
+++ src/Util/Cli/AbstractCommandHandler.ts
@@ -4,5 +4,5 @@ import type { AR } from '@hexancore/common';
 export abstract class AbstractCommandHandler<O extends Record<string, any>> {
   public constructor(protected helpers: CommonHelpers) { }
 
-  public abstract execute(options: O, args: string[]): AR<void>;
+  public abstract execute(options: O, ...args: string[]): AR<void>;
 }
